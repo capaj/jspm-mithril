440f55bd6274750976429a1f128cb5b94afa1ba6
Jiri Spac <capajj@gmail.com>
2015-12-12 01:48:50 +0100

    updated to systemjs-hot-reloader and better example with m.route

--- public/app.js
+++ public/app.js
@@ -1,33 +1,37 @@
 import m from 'mithril'
 
-
 var Demo = {
-	//controller
-	controller: function() {
-		return {
-			pages: [
-        {"title": "Getting Started", "url": "getting-started.html"},
-        {"title": "Documentation", "url": "mithril.html"},
-        {"title": "Mithril Blog", "url": "http://lhorie.github.io/mithril-blog/"},
-        {"title": "Mailing List", "url": "https://groups.google.com/forum/#!forum/mithriljs"}
+  // controller
+  controller: function () {
+    return {
+      pages: [
+        {'title': 'Getting Started', 'url': 'getting-started.html'},
+        {'title': 'Documentation', 'url': 'mithril.html'},
+        {'title': 'Mithril Blog', 'url': 'http://lhorie.github.io/mithril-blog/'},
+        {'title': 'Mailing List', 'url': 'https://groups.google.com/forum/#!forum/mithriljs'}
       ],
-			log: function() {
-				console.log('clicked')
-			}
-		}
-	},
+      log: function () {
+        console.log('clicked')
+      }
+    }
+  },
 
-	//view
-	view: function(ctrl) {
-		return m("div", {className: 'list'}, [
-			ctrl.pages.map(function(page) {
-				return m("div", [m("a", {href: page.url}, page.title)])
-			}),
-			m("button", {onclick: ctrl.log}, "console log test")
-		])
-	}
+  // view
+  view: function (ctrl) {
+    return m('div', {className: 'list'}, [
+      ctrl.pages.map(function (page) {
+        return m('div', [m('a', {href: page.url}, page.title)])
+      }),
+      m('button', {onclick: ctrl.log}, 'console log test')
+    ])
+  }
 }
 
+// initialize
+m.route(document.getElementById('app'), '/', {
+  '/': Demo
+})
 
-//initialize
-m.mount(document.getElementById("app"), Demo)
+if (System.trace) {
+  m.route(m.route())	// little hack for hot reload to work with mithrill router
+}
